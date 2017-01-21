30f9f666e47a9de4f670a1f077490b520cf483a5
Quentin Penault <quentinpenault@live.fr>
2017-01-21 01:55:42 +0100

    script c# player

---
+++ player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour
+{
+
+    public Transform spawn;
+    public double pointLife = 100;
+    public bool isAlive = true;
+
+    void Start()
+    {
+        this.transform.position = spawn.transform.position;
+    }
+
+
+    void Update()
+    {
+
+        if (pointLife <= 0) { isAlive = false; } else { isAlive = true; }
+    }
+
+    void getDamage(double damages)
+    {
+        this.pointLife -= damages;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "ennemy")
+        {
+            getDamage(25);
+            print("took damages !");
+        }
+    }
+
+}
