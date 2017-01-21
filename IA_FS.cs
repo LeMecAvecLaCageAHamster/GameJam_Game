82310f428b55987a5bd252fb5418a3f1c2f9b630
Pierre Lezan <1pierrelezan@gmail.com>
2017-01-21 15:07:43 +0100

    adding AI

---
+++ IA_FS.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA_FS : MonoBehaviour {
+
+    public float range;
+    public Transform target;
+    public float speed;
+    public float jump = 2;
+    public Rigidbody2D rb;
+
+    private Vector2 movement;
+    private Vector3 jumping = new Vector3(0, 1,0);
+    // Use this for initialization
+    void Start () {
+        rb = this.GetComponent<Rigidbody2D>();
+        print(rb);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        movement = Vector2.MoveTowards(transform.position, target.position, step);
+        //movement.y = 0;
+        //transform.position = movement;
+        //movement.y = 0;
+        //print("movement x = " + (movement.x - transform.position.x));
+        transform.Translate(movement.x-transform.position.x,0,0);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Platform")
+        {
+            //transform.AddForce(new Vector3(0, 100, 0), ForceMode.Impulse);
+            print("should jump");
+            //rb.AddForce(Vector3.up * jump);
+            rb.AddForce(Vector2.up*jump, ForceMode2D.Impulse);
+        }
+    }
+
+}
---
+++ IA_Goule.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA_Goule : MonoBehaviour {
+
+    //Public values
+    public float range = 0.5f;
+    public Transform LIAL;
+    public Transform LIAR;
+    private float sensDeplacement = -0.02f;
+    
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        if (sensDeplacement < 0 && Vector3.Distance(this.transform.position,LIAL.position ) < range){
+            sensDeplacement = 0.02f;
+        }else if (sensDeplacement > 0 && Vector3.Distance(this.transform.position, LIAR.position) < range){
+            sensDeplacement = -0.02f;
+        }
+
+        transform.Translate(sensDeplacement,0,0);
+    }
+}
