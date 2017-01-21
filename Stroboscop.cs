255ce59d9dbb44c67ba9eb850791d3cee2c553e7
Quentin Penault <quentinpenault@live.fr>
2017-01-21 19:23:49 +0100

    create stroboscop --> source energy

---
+++ Stroboscop.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stroboscop : MonoBehaviour {
+
+    public Light light;
+	
+	void Update () {
+        light.range = Random.Range(5 ,20);
+    }
+}
