edfa5604717bf451e45054bf2107bc7e2308dccf
Pierre Lezan <1pierrelezan@gmail.com>
2017-01-22 07:32:12 +0100

    spider-web

---
+++ spider_web.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spider_web : MonoBehaviour {
+
+    public IA_Follower mob;
+    public float range = 0.01f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(this.transform.position, mob.transform.position) < range)
+        {
+            //mob.transform.RigidbodyConstraint = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+            mob.speed = 0f;
+        }
+    }
+}
