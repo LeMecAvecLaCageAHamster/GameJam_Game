87b07dcd257bf721ceeccd59d10ec28acf392991
Blames <nathan.scarsa@gmail.com>
2017-01-22 04:15:46 +0100

    add deffierentes scene  + link menu/scene1

---
+++ loadLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadLevel : MonoBehaviour {
+
+	// Use this for initialization
+	public void OnClick () {
+		Application.LoadLevel ("scene1");
+		Screen.lockCursor = true;
+	}
+
+
+
+}
